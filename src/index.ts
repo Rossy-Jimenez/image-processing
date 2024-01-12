ec58e42b4fb73b8df6800af3ef5370db651f32f9
Rossy-Jimenez <146148675+Rossy-Jimenez@users.noreply.github.com>
2024-01-11 18:44:49 -0600

    Corrigiendo barra de navegacion

--- src/index.ts
+++ src/index.ts
@@ -661,75 +661,7 @@ document.getElementById('files').addEventListener('change', imgLocal.handleFileS
 document.getElementById('files2').addEventListener('change', imgLocal4.handleFileSelect, false);
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', imgLocal.handleFileSelect, false);
-//menu op basicas
-document.getElementById("op-gris").addEventListener('click', convertirAGris, false);
-document.getElementById("op-negativo").addEventListener('click', convertirANegativo, false);
-document.getElementById("op-neg-gris").addEventListener('click', convertirANegativoGrises, false);
-document.getElementById("op-rojo").addEventListener('click', convertirARojo, false);
-document.getElementById("op-verde").addEventListener('click', convertirAVerde, false);
-document.getElementById("op-azul").addEventListener('click', convertirAAzul, false);
-document.getElementById("op-tricolor").addEventListener('click', convertirTricolor, false);
-document.getElementById("op-gamma").addEventListener('click', correccionGamma, false);
-document.getElementById("op-umbral1").addEventListener('click', umbralizado, false);
-document.getElementById("op-umbral-2-limites").addEventListener('click', umbral2limites, false);
-document.getElementById("op-desfaseX").addEventListener('click', desfaseX, false);
-document.getElementById("op-desfaseY").addEventListener('click', desfaseY, false);
-document.getElementById("op-desfaseD").addEventListener('click', desfaseD, false);
-//menu op. edicion
-document.getElementById("op-brillo").addEventListener('click', changeBrightness, false);
-document.getElementById("op-ftrans").addEventListener('click', cambioFtransferencia, false);
-document.getElementById("op-gradienteX").addEventListener('click', colorGradienteX, false);
-document.getElementById("op-gradienteY").addEventListener('click', colorGradienteY, false);
-document.getElementById("op-contraste").addEventListener('click', opchangeContraste, false);
-document.getElementById("op-falsocolor").addEventListener('click', opchangeFalsoColor, false);
 
-//op matematicas
-document.getElementById("op-pow").addEventListener('click', opgetPow, false);
-document.getElementById("op-sqrt").addEventListener('click', sqrt, false);
-document.getElementById("op-sine").addEventListener('click', funcionSine, false);
-document.getElementById("op-cos").addEventListener('click', coseno, false);
-document.getElementById("op-tan").addEventListener('click', tan, false);
-document.getElementById("op-add").addEventListener('click', add, false);
-document.getElementById("op-subtract").addEventListener('click', subtract, false);
-document.getElementById("op-multiplicacion").addEventListener('click', multiplicacion, false);
-document.getElementById("op-div").addEventListener('click', div, false);
-
-
-//op con imagenes compuestas
-document.getElementById("op-addimg").addEventListener('click', sumaImg, false);
-document.getElementById("op-marca-agua-centro").addEventListener('click', marcaAguaCentro, false);
-document.getElementById("op-marca-agua-array").addEventListener('click', marcaAguaArray, false);
-
-//op con efectos
-document.getElementById("op-rain").addEventListener('click', rain, false);
-document.getElementById("op-rain2").addEventListener('click', rain2, false);
-
-//op con texto.
-document.getElementById("op-text").addEventListener('click', textEfects, false);
-
-//histogramas
-document.getElementById("op-hist").addEventListener('click', histogramas, false);
-document.getElementById("op-ecualizar").addEventListener('click', ecualizado, false);
-
-
-//mortfologia
-document.getElementById("op-eros").addEventListener('click', erosionarImg, false);
-document.getElementById("op-dila").addEventListener('click', dilatarImg, false);
-document.getElementById("op-aper").addEventListener('click', aperturaImg, false);
-document.getElementById("op-cier").addEventListener('click', cierreImg, false);
-
-//operacion con imagenes siteticas
-document.getElementById("op-pulso").addEventListener('click', generarPulso, false);
-document.getElementById("op-ruido").addEventListener('click', generarRuido, false);
-document.getElementById("op-rampax").addEventListener('click', generarRampaX, false);
-document.getElementById("op-rampay").addEventListener('click', generarRampaY, false);
-
-//operaciones geometricas
-document.getElementById("op-escalamiento").addEventListener('click', escalarImagen2, false);
-document.getElementById("op-rotacion").addEventListener('click', rotarImagen2, false);
-document.getElementById("op-shearingX").addEventListener('click', shearingX, false);
-document.getElementById("op-shearingY").addEventListener('click', shearingY, false);
-document.getElementById("op-afin").addEventListener('click', tAfin, false);
 
 //proyecto 
 document.getElementById("EfectoHDR").addEventListener('click', EfectoHDR);
@@ -743,4 +675,4 @@ document.getElementById("EfectoMovimientoVertical").addEventListener('click', Ef
 document.getElementById("efectoMovimientoDiagonal").addEventListener('click', EfectoMovimientoDiagonal);
 document.getElementById("efectoBurbujas").addEventListener('click', iniciarBurbujas);
 document.getElementById("iniciarLluviaBurbujas").addEventListener('click', iniciarLluviaBurbujas);
-document.getElementById('iniciarLluviaOndas').addEventListener('click', iniciarLluviaOndas);
+document.getElementById("iniciarLluviaOndas").addEventListener('click', iniciarLluviaOndas);
